16c0f2455750feebfc30ddda0bdf487260f7db13
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-19 07:49:59 -0300

    2do commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo!!!");
+        System.out.println("Estoy aprendiendo GIT, los commits y los push!");
     }
 }
